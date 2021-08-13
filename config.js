226a3f60e5b1067222e128e04097ac533ee0fd82
wdklsn <88869371+wdklsn@users.noreply.github.com>
2021-08-13 10:44:43 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,16 @@
 const CONFIG = {
-    introTitle: 'Helu Miii',
-    introDesc: ' là Cường nèeeee',
-    btnIntro: 'bất ngờ honggg',
-    title: 'Đố Mi biết sáng nay của C như nàooo',
-    desc: 'Để ncoi Mi đoán đúng hong nhen',
-    btnYes: 'Nhớ Mi',
-    btnNo: 'Hong bít',
-    question:'đố Mi biết mai là ngày gì nhennn',
-    mess: 'sao Mi đoán đúng ròi nèeee',
-    
+    introTitle: 'Heluuu Miiiiii',
+    introDesc: `Đố Mi biết sáng nay dậy C thấy saooo`,
+    btnIntro: 'để ncoi đoán đúng hong nhen',
+    title: 'nè Mi thử coi coi nsao',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Chắc là thấy nhớ Mi',
+    btnNo: 'Hỏng thấy gì hết chơn',
+    question:'Đố Mi biết mai là ngày gì luôn ớ',
+    btnReply: 'Để coi đúng hong ',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
