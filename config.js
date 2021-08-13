e1f53300ea40c57c406a456835df3a847191744c
wdklsn <88869371+wdklsn@users.noreply.github.com>
2021-08-13 10:39:44 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,9 +3,9 @@ const CONFIG = {
     introDesc: ' là Cường nèeeee',
     btnIntro: 'bất ngờ honggg',
     title: 'Đố Mi biết sáng nay của C như nàooo',
-    desc: 'Đố Mi biết mai ngày gì luôn ớ',
+    desc: 'Để ncoi Mi đoán đúng hong nhen',
     btnYes: 'Nhớ Mi',
     btnNo: 'Hong bít',
     question:'đố Mi biết mai là ngày gì nhennn',
-    mess: 'sao Mi đoán đúng ròi',
+    mess: 'sao Mi đoán đúng ròi nèeeee',
     
